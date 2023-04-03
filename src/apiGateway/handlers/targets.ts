2d102d11af84a282bda6536f85ac036d48c7acf6
Elias Youssef <eliasyoussef47@gmail.com>
2023-04-03 16:42:18 +0200

    Sleep function. Reset breaker configs.

--- src/apiGateway/handlers/targets.ts
+++ src/apiGateway/handlers/targets.ts
@@ -4,7 +4,7 @@ import Targets, { StoreArgs } from "@/apiGateway/businessLogic/targets.js";
 import { attachStandardCircuitBreakerCallbacks } from "@/shared/utils/CircuitBreaker.js";
 
 const circuitBreakerOptions: CircuitBreakerOptions = {
-	timeout: 10000, // If our function takes longer than 3 seconds, trigger a failure
+	timeout: 3000, // If our function takes longer than 3 seconds, trigger a failure
 	errorThresholdPercentage: 50, // When 50% of requests fail, trip the circuit
 	resetTimeout: 3000 // After 3 seconds, try again.
 };
