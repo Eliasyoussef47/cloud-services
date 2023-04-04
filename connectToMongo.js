c545c870e263734b7ef8aeb46217108454bbe32c
nielsverheggen <nielsvh23@gmail.com>
2023-04-04 17:40:18 +0200

    try

---
+++ connectToMongo.js
@@ -0,0 +1,15 @@
+const { MongoClient } = require('mongodb');
+
+const uri = 'mongodb://localhost:27017';
+const client = new MongoClient(uri);
+
+async function connectToMongoDB() {
+	try {
+		await client.connect();
+	} catch (error) {
+		console.error(error);
+	} finally {
+		// await client.close();
+	}
+}
+connectToMongoDB().then(() => {});
